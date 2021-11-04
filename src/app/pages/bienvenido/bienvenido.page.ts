fa8873a418a2400dd7b4774f6f66c9d8effe7c1e
jair alejandro <jairalejandro32@outlook.com>
2021-11-04 02:40:34 -0600

    Segunda interfax

--- src/app/pages/bienvenido/bienvenido.page.ts
+++ src/app/pages/bienvenido/bienvenido.page.ts
@@ -1,23 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';  
 
 @Component({
   selector: 'app-bienvenido',
   templateUrl: './bienvenido.page.html',
   styleUrls: ['./bienvenido.page.scss'],
 })
-export class BienvenidoPage implements OnInit {
-
+export class BienvenidoPage {
   constructor(
+    public alertCtrl: AlertController,
     private router: Router
   ) { }
-
-  ngOnInit() {
-  }
-
   capturarB(){
-    console.log('jhkhjj');
-
     this.router.navigateByUrl('bitacora');
   }
-}
+  async showAlert() {  
+    const alert = await this.alertCtrl.create({  
+      header: 'BitacorApp',  
+      subHeader: 'Que tenga un buen día',  
+      buttons: ['OK']  
+    });  
+    await alert.present();
+  }  
+}  
