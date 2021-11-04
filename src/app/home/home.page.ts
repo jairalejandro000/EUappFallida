7e46e9ed9f9bacb08ca5aba0f17c0bce11b95417
jair alejandro <jairalejandro32@outlook.com>
2021-11-04 02:05:40 -0600

    Login con diseño

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,20 +8,24 @@ import { MenuController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  show = true;
+  a = 'Bitacoras';
   constructor(
     private menu: MenuController,
-    private router: Router
+    private router: Router,
   ) {}
-
   openFirst() {
     this.menu.enable(true, 'first');
     this.menu.open('first');
   }
   registrar(){
-    console.log('j');
     this.router.navigateByUrl('bienvenido');
   }
-
-
+  buttonClicked(){
+    this.show =! this.show;
+    window.location.reload();
+  }
+  titleClicked(){
+    this.a = '';
+  }
 }
